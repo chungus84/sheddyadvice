38035f0e8b3f0615aecc0fec874de428d69e8af4
Ksenia Koneva <hello@xeniakoneva.co.uk>
2022-08-20 11:34:38 +0100

    controller skeleton

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end
