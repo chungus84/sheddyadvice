5896cc73e319687fb11e7f45c20fb39c7035ee4e
Kenichi Beveridge <kenbev84@gmail.com>
2022-08-20 16:52:46 +0100

    Added further migration to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_20_144442)
+ActiveRecord::Schema[7.0].define(version: 2022_08_20_152211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "feedbacks", force: :cascade do |t|
     t.bigint "user_id", null: false
