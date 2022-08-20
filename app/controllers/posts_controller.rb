f21e41b52e44f048bd9df35fe2af6b3e5a66b3a5
Harrie <mayhemcreates@gmail.com>
2022-08-20 12:45:11 +0100

    change to create in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new
-    if post.save
+    if @post.save
       redirect_to posts_path
     else
       render :new, status: :unprocessable_entity
