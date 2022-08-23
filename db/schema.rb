80b025e5845539997454c1c2a9f21dc226828548
Ksenia Koneva <hello@xeniakoneva.co.uk>
2022-08-23 19:07:03 +0100

    adds category to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_20_160930) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_180619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_20_160930) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.string "video"
+    t.string "category"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
