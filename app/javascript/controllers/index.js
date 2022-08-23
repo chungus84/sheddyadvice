f1f39d3688597fcc7c26abffc174ceb889ec9a87
Ksenia Koneva <hello@xeniakoneva.co.uk>
2022-08-23 21:38:03 +0100

    Adds chatroom cable

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,13 +4,14 @@
 
 import { application } from "./application"
 
-
 import AddReviewInListController from "./add_review_in_list_controller"
 application.register("add-review-in-list", AddReviewInListController)
 
+import ChatroomSubscriptionController from "./chatroom_subscription_controller"
+application.register("chatroom-subscription", ChatroomSubscriptionController)
+
 import EditFieldsController from "./edit_fields_controller"
 application.register("edit-fields", EditFieldsController)
-
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
