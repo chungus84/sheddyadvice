ad5fd0db05cc23b9eff2c28990549f7b91cff62b
Kenichi Beveridge <kenbev84@gmail.com>
2022-09-03 12:03:39 +0100

    Add styling for index cards

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,9 @@ class PostsController < ApplicationController
 
   def index
     if params[:query].present?
-      @posts = Post.search_by_title_and_body(params[:query])
+      @posts = Post.includes(:user).search_by_title_and_body(params[:query])
     else
-      @posts = Post.all
+      @posts = Post.includes(:user).all
     end
 
   end
