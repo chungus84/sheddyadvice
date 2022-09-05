3e8ed69dc8218cf76db2b78bde812364683afcb8
Kenichi Beveridge <kenbev84@gmail.com>
2022-09-05 21:01:15 +0100

    Add homepage cards

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  acts_as_voter
+  acts_as_votable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
