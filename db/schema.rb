5dd34488cb1ce02b390b037a63069fa56010454b
Harrie <mayhemcreates@gmail.com>
2022-09-11 10:58:55 +0100

    fixed card styling for home page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_09_10_161029) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
