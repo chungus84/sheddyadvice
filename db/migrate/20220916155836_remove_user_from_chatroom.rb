d44846bf9433e539c59982404b2c2fe661016443
Harrie <mayhemcreates@gmail.com>
2022-09-17 10:57:54 +0100

    styled list index page

--- db/migrate/20220916155836_remove_user_from_chatroom.rb
+++ db/migrate/20220916155836_remove_user_from_chatroom.rb
@@ -1,5 +1,5 @@
 class RemoveUserFromChatroom < ActiveRecord::Migration[7.0]
   def change
-    remove_column :chatrooms, :user_id
+    # remove_column :chatrooms, :user_id
   end
 end
--- db/migrate/20220916161452_add_new_users_chatroom.rb
+++ db/migrate/20220916161452_add_new_users_chatroom.rb
@@ -1,10 +1,10 @@
 class AddNewUsersChatroom < ActiveRecord::Migration[7.0]
   def change
-    add_column :chatrooms, :user_one, :integer
-    add_column :chatrooms, :user_two, :integer
-    add_foreign_key :chatrooms, :users, column: :user_one
-    add_foreign_key :chatrooms, :users, column: :user_two
-    add_index :chatrooms, :user_one
-    add_index :chatrooms, :user_two
+    # add_column :chatrooms, :user_one, :integer
+    # add_column :chatrooms, :user_two, :integer
+    # add_foreign_key :chatrooms, :users, column: :user_one
+    # add_foreign_key :chatrooms, :users, column: :user_two
+    # add_index :chatrooms, :user_one
+    # add_index :chatrooms, :user_two
   end
 end
